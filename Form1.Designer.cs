06bad37e15a6aade2950d65a8f7608464ca77adc
localcChicken <42898191+localcChicken@users.noreply.github.com>
2018-11-04 16:46:08 +0300

    Release
    Release

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFolder = new System.Windows.Forms.Button();
+            this.Generate = new System.Windows.Forms.Button();
+            this.SelectFile = new System.Windows.Forms.Button();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // SelectFolder
+            // 
+            this.SelectFolder.Location = new System.Drawing.Point(225, 58);
+            this.SelectFolder.Name = "SelectFolder";
+            this.SelectFolder.Size = new System.Drawing.Size(132, 36);
+            this.SelectFolder.TabIndex = 0;
+            this.SelectFolder.Text = "Select directory";
+            this.SelectFolder.UseVisualStyleBackColor = true;
+            this.SelectFolder.Click += new System.EventHandler(this.SelectFolder_Click);
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(4, 12);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(353, 40);
+            this.Generate.TabIndex = 1;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // SelectFile
+            // 
+            this.SelectFile.Location = new System.Drawing.Point(4, 58);
+            this.SelectFile.Name = "SelectFile";
+            this.SelectFile.Size = new System.Drawing.Size(132, 36);
+            this.SelectFile.TabIndex = 2;
+            this.SelectFile.Text = "Select output file";
+            this.SelectFile.UseVisualStyleBackColor = true;
+            this.SelectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // saveFileDialog2
+            // 
+            this.saveFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog2_FileOk);
+            // 
+            // folderBrowserDialog2
+            // 
+            this.folderBrowserDialog2.HelpRequest += new System.EventHandler(this.folderBrowserDialog2_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(359, 100);
+            this.Controls.Add(this.SelectFile);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.SelectFolder);
+            this.Name = "Form1";
+            this.Text = "Image parser";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button SelectFolder;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.Button SelectFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public String sourceDir;
+        public String outputFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void SelectFolder_Click(object sender, EventArgs e)
+        {
+            using(var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    sourceDir = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            using (var saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JSON File|*.json";
+                saveFileDialog.Title = "Select file";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    outputFile = saveFileDialog.FileName;
+                }
+            }
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(sourceDir);
+            GenerateJson(outputFile, sourceDir);
+        }
+        private void GenerateJson(String output, String input)
+        {
+            dynamic GeneratedArr = new JObject();
+            GeneratedArr.images = new JArray();
+            try
+            {
+                string[] files = System.IO.Directory.GetFiles(input, "*.png");
+                foreach (string element in files)
+                {
+                    Bitmap image = new Bitmap(element);
+                    bool isOpaque = false;
+                    for (int x = 1; x <= image.Width; x++)
+                    {
+                        for (int y = 1; y <= image.Height; y++)
+                        {
+                            if (image.GetPixel(x - 1, y - 1).A <= 254)
+                            {
+                                isOpaque = true;
+                            }
+                        }
+                    }
+                    if (isOpaque == true)
+                    {
+                        GeneratedArr.images.Add(element.ToString().Substring(sourceDir.Length + 1));
+                    }
+
+                }
+                try
+                {
+                    System.IO.File.WriteAllText(outputFile, GeneratedArr.ToString());
+                    MessageBox.Show("Done!");
+                }
+                catch (System.ArgumentNullException)
+                {
+                    MessageBox.Show("Output file not selected!");
+                }
+            } catch(System.ArgumentNullException)
+            {
+                MessageBox.Show("Input folder not selected!");
+            }
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog2_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
